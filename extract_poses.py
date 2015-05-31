adcc0fa10c1a8935e8a33ae9c84086de56b47f93
DylanAuty <dylan.auty@gmail.com>
2015-05-31 18:46:14 +0100

    Successfully added in a keyframe to all F-Curves

--- extract_poses.py
+++ extract_poses.py
@@ -5,9 +5,26 @@
 # Aims to save time rather than manually exporting all values...
 
 import bpy
+import json
 
 action = bpy.data.actions['ColdBake']
 for fc in action.fcurves:
-    print(str(fc.data_path) + " channel " + str(fc.array_index))
-    for keyframe in fc.keyframe_points:
-        print(str(keyframe.co))
+	print(str(fc.data_path) + " channel " + str(fc.array_index))
+	print("F-CURVE: " + str(fc)) #Does this mean anything?
+	for keyframe in fc.keyframe_points:
+		print("KEYFRAME: " + str(keyframe)) # No idea if this'll do anything worthwhile
+		# keyframe consists of a control point, left handle and right handle
+		# their coordinates on the F-Curve graph can be accessed using .co, .handle_left, and .handle_right respectively
+		print(str(keyframe.co.x) + ", " + str(keyframe.co.y))
+
+print ("DONE PRINTING...")
+
+action2 = bpy.data.actions['PythonTest']	# Gonna try and insert a keyframe everywhere, value 5, frame 200
+for fc in action2.fcurves:
+	print(str(fc.data_path) + " channel " + str(fc.array_index))
+	# First insert a keyframe for everything at the start...
+	fc.keyframe_points.insert(frame=200, value=2, options={'REPLACE'})
+	# Now print out all keyframes...
+	for keyframe in fc.keyframe_points:
+		print("KEYFRAME: " + str(keyframe))
+		print(str(keyframe.co.x) + ", " + str(keyframe.co.y))
