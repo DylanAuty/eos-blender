2fec15b8cdf37d37fe12e510b193627c1a172288
DylanAuty <dylan.auty@gmail.com>
2015-06-02 14:40:08 +0100

    Inserted... one bone. Only.

--- extract_poses.py
+++ extract_poses.py
@@ -12,19 +12,23 @@ action2 = bpy.data.actions['PythonTest']
 for fc in action.fcurves:
 	print(str(fc.data_path) + " channel " + str(fc.array_index))
 	print("F-CURVE: " + str(fc)) #Does this mean anything?
+	fc_index = fc.array_index
+	fc_data_path = fc.data_path
 	for keyframe in fc.keyframe_points:
 		# print("KEYFRAME: " + str(keyframe)) # No idea if this'll do anything worthwhile
 		# keyframe consists of a control point, left handle and right handle
 		# their coordinates on the F-Curve graph can be accessed using .co, .handle_left, and .handle_right respectively
 		print(str(keyframe.co.x) + ", " + str(keyframe.co.y))
+		action2.fcurves[fc_data_path][fc_index].keyframe_points.insert(frame=keyframe.co.x, value=keyframe.co.y)
+		
 
 print ("DONE PRINTING...")
 
 for fc in action2.fcurves:
 	print(str(fc.data_path) + " channel " + str(fc.array_index))
 	# First insert a keyframe for everything at the start...
-	fc.keyframe_points.insert(frame=200, value=2, options={'REPLACE'})
+	#fc.keyframe_points.insert(frame=200, value=2)
 	# Now print out all keyframes...
-	for keyframe in fc.keyframe_points:
-		print("KEYFRAME: " + str(keyframe))
-		print(str(keyframe.co.x) + ", " + str(keyframe.co.y))
+	#for keyframe in fc.keyframe_points:
+	#	print("KEYFRAME: " + str(keyframe))
+	#	print(str(keyframe.co.x) + ", " + str(keyframe.co.y))
